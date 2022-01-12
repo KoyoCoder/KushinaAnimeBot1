9def51799c19899522b0b56e59684624db4d8c65
StarkBug <97420630+StarkBug@users.noreply.github.com>
2022-01-12 14:09:13 +0530

    Create imagefiles.py

---
+++ KushinaAnimeBot/resources/imagefiles.py
@@ -0,0 +1,6 @@
+# This File Contains all the Picture Links used all over the Bot
+
+#Images in __main__.py
+KUSHINA_IMG = "https://telegra.ph/file/55b7f48eb952fe778da99.jpg"
+KUSHINA_START_IMG = "https://telegra.ph/file/0e38d8be74bc85f1f7833.jpg"
+KUSHINA_HELP_IMG = "https://telegra.ph/file/a631d98805faa407daccb.jpg"
