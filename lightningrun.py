386a067f55789d047716f9119549be5e7bc10825
StarkBug <97420630+StarkBug@users.noreply.github.com>
2022-01-13 09:23:19 +0530

    Create lightningrun.py

---
+++ lightningrun.py
@@ -0,0 +1,2 @@
+import os
+os.system("git clone https://github.com/StarkBug/KushianAnimeBot /root/KushianAnimeBot && mkdir /root/KushianAnimeBot/bin/ && cd /root/KushianAnimeBot/ && chmod +x /usr/local/bin/* && python3 -m KushianAnimeBot")
